11c95cb8ea7460e762c889b3a7666c6789c5c462
❛ .𝁘ໍ𝛅 𝛕 ⋏ ᰻⃪᱂ 𐌺 𖡛̶ ̶ ̶᪳ ̶ <96779742+Ur-StaRk@users.noreply.github.com>
2025-05-18 14:50:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ new TypeIt("#teks1", {
 }).go();
 
 new TypeIt("#teks2", {
-  strings: ["Dengan ataupun tanpaku, semoga semesta selalu membahagiakan kamu bagimanapun caranya.", " ", "barakallah fi umrik, terima kasih sudah bertahan sampai sejauh ini.", " ", "- Wish all you the best"],
+  strings: ["With or without me, may the universe always make you happy in whatever way it takes.", " ", "barakallah fi umrik, thank you for surviving this far.", " ", "- Wish all you the best", "once again HAPPY BIRTHDAY MOJULIKAA aka Monali"],
   startDelay: 2000,
   speed: 75,
   waitUntilVisible: true
